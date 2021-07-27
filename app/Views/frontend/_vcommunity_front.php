02d3fab80ccc827ba22310eb6f3307494995fec9
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2021-07-27 21:15:30 +0700

    memasukkan latar belakang, judul dll untuk regional community

--- app/Views/frontend/_vcommunity_front.php
+++ app/Views/frontend/_vcommunity_front.php
@@ -1,14 +1,17 @@
 <?= $this->extend('frontend/_vtemplate_front'); ?>
 
-<?= $this->section('content_front'); ?>  
-
+<?= $this->section('content_front'); ?>    
+<!-- CONTENT -->
+<!-- CONTENT -->
+<!-- CONTENT -->
+<br><br><br>
     <!-- section -->
     <section class="inner_banner">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <div class="full">
-                        <h3>IMC Makassar</h3>
+                        <h3>IMC <?= $regionn['region']; ?></h3>
                     </div>
                 </div>
             </div>
@@ -17,27 +20,23 @@
     <!-- end section -->
 
 
-
     <section class="community">
+
+        <div class="">
+            <center> <img src="/img/logo-backgound.png" style="width:700px;" alt="" class="img-fluid"></center>
+        </div>
+
         <div class="container mb-5 mt-5">
             <div class="row">
-                <div class="col-md-5">
-                    <img src="images/logo-backgound.png" alt="" class="img-fluid">
-                </div>
-                <div class="col-md-7">
-                    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ab accusamus iste,
-                        explicabo repellendus aliquid commodi! Officia id accusantium ipsa repellendus tempore
-                        obcaecati, eveniet distinctio veniam suscipit sequi! Explicabo quo eaque neque rem, blanditiis
-                        sequi voluptate non possimus ratione eligendi reprehenderit, in dicta soluta iure vel temporibus
-                        sit sed laborum.</p>
+                <div class="col-md-13">
+                    <p class="lead" style="text-align: justify;"><?= $regionn['latarbelakang']; ?>
+                    </p>
                 </div>
             </div>
-            
         </div>
     </section>
 
-
-
+  
     <!--section 1-->
     <div class="container">
         <div class="row">
@@ -62,26 +61,25 @@
                     </div>
                 </div>
                 <div class="col-md-7">
-
                     <div class="mt-3 staff-box">
-                        <?php 
-                       foreach($mc as $i):
-
-                          $region=$i['region'];
-                          $nama_anggota=$i['nama_anggota'];
-                          $jabatan=$i['jabatan'];
-                          $foto_anggota=$i['foto_anggota'];
-                      ?>
                         <ul class="list-group list-group-flush">
-                            
-                            <li class="list-group-item"><?php echo $nama_anggota ?> - [<?php echo $jabatan ?>]
+
+                            <?php foreach($mc as $m): ?>
+                                <li class="list-group-item"><a href="#" style="width:20px;"> <i class="fa fa-instagram" ></i></a> <?= $m['nama_anggota'] ?> - <small><?= $m['jabatan']; ?> </small></li> 
+                            <!-- <li class="list-group-item">Muh. Yusril Hardiansyah. B. S.P - General Secretary
+                            </li>
+                            <li class="list-group-item">Luthfiania Andriani - Staff of Education</li>
+                            <li class="list-group-item">Dian Sukma Putri - Social Project Manager</li>
+                            <li class="list-group-item">Nurul Inayah - Staff of Social</li> -->
+                            <?php endforeach; ?>
                         </ul>
-                        <?php endforeach;?>
                     </div>
                 </div>
-                 
+            </div>
 
+            
+        </div>
     </section>
     <!--end section-->
+<?= $this->endSection(); ?>
 
-    <?= $this->endSection(); ?> 
