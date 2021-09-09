be504a29119b21e1723feab2aace5786fa56e50f
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2021-09-09 10:58:04 +0700

    edit responsive

--- app/Views/frontend/_vtemplate_front.php
+++ app/Views/frontend/_vtemplate_front.php
@@ -39,7 +39,7 @@
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 
     <!-- PAGES ABOUT RESPONSIVE -->
-    <link rel="stylesheet" href="/assets/css/about-eka.css">
+    <link rel="stylesheet" href="/assets/css/responsive-eka.css">
 
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
